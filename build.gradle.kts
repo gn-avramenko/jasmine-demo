6da27e3619f99205d9eeb0658ca966668baf1079
flint80@yandex.ru <flint80@yandex.ru>
2020-04-14 20:45:39 +0300

    repair

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,20 @@
-//test2
+import com.gridnine.jasmine.gradle.plugin.jasmine
+
+buildscript {
+    dependencies{
+        "classpath"(files("submodules/jasmine/build-scripts/build/classes/kotlin/main"))
+        "classpath"(files("submodules/jasmine/lib/spf-1.0.jar"))
+    }
+}
+
+apply<com.gridnine.jasmine.gradle.plugin.JasmineConfigPlugin>()
+
+jasmine {
+    kotlinVersion = "1.3.71"
+    plugins("submodules/jasmine/plugins") {
+        plugin("com.gridnine.jasmine.server.core")
+    }
+}
+
+apply<com.gridnine.jasmine.gradle.plugin.JasminePlugin>()
+
