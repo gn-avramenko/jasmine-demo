5c61bdac373c70cbb633476a0285f85230bc525b
flint80@yandex.ru <flint80@yandex.ru>
2020-09-21 18:13:20 +0300

    web

--- plugins/com.gridnine.jasmine.web.demo/source/com/gridnine/jasmine/web/demo/admin/app/jasmine-demo-admin-app.kt
+++ plugins/com.gridnine.jasmine.web.demo/source/com/gridnine/jasmine/web/demo/admin/app/jasmine-demo-admin-app.kt
@@ -6,13 +6,11 @@
 package com.gridnine.jasmine.web.demo.admin.app
 
 import com.gridnine.jasmine.server.standard.model.rest.GetWorkspaceRequestJS
-import com.gridnine.jasmine.web.core.CoreWebMessagesJS
 import com.gridnine.jasmine.web.core.StandardRestClient
 import com.gridnine.jasmine.web.core.activator.CoreActivatorJS
 import com.gridnine.jasmine.web.core.mainframe.MainFrame
 import com.gridnine.jasmine.web.core.remote.StandardRpcManager
 import com.gridnine.jasmine.web.core.ui.UiLibraryAdapter
-import com.gridnine.jasmine.web.core.ui.components.LinkButtonConfiguration
 import com.gridnine.jasmine.web.core.ui.components.MenuButtonConfiguration
 import com.gridnine.jasmine.web.core.ui.components.StandardMenuItem
 import com.gridnine.jasmine.web.easyui.activator.EasyUiActivator
@@ -29,16 +27,20 @@ fun main() {
     EasyUiActivator().configure(config)
     coreActivator.activate().then { StandardRestClient.standard_standard_getWorkspace(GetWorkspaceRequestJS())}.then {
         var mainFrame = MainFrame()
-        mainFrame.tools.add(MenuButtonConfiguration{
-            title = CoreWebMessagesJS.settings
-            elements.add(StandardMenuItem{
-                title = "Выход"
-                handler = {
-                    window.alert("Выход")
-                }
+        mainFrame.configure {
+            tools.add(MenuButtonConfiguration{
+                icon = "core:settings"
+                elements.add(StandardMenuItem{
+                    title = "Выход"
+                    handler = {
+                        window.alert("Выход")
+                    }
+                })
             })
-        })
-        mainFrame.initialize(it.workspace)
+
+
+        }
+        mainFrame.build(it.workspace)
         UiLibraryAdapter.get().showWindow(mainFrame)
     }
 }
